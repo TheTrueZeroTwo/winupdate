d58687bd410501fb96b559b9c9774b283b4b2403
Zero Two <85081508+TheTrueZeroTwo@users.noreply.github.com>
2023-09-15 15:11:53 -0600

    Update menu.ps1
    added remove printer and catch statements

--- menu.ps1
+++ menu.ps1
@@ -49,7 +49,7 @@ function Download-File {
         $webClient.DownloadFile($Url, $Destination)
         Write-Output "File downloaded successfully to $($Destination)"
     } catch {
-        Write-Error "Failed to download file: $_.Exception.Message"
+        Write-Error "Failed to download file: $($_.Exception.Message)"
     }
 
     if (!(Test-Path $Destination)) {
@@ -57,7 +57,50 @@ function Download-File {
     }
 }
 
+function Remove-NonDefaultPrintersAndDrivers {
+    <#
+    Removes non-default printers and their drivers using the PrintManagement module.
+    Excludes Microsoft drivers, OneNote, Nitro PDF, fax, and PDF printers.
+    #>
 
+    try {
+        # Enumerate all printers and filter out non-default printers to remove
+        $printers = Get-Printer | Where-Object {
+            -not $_.IsDefault -and
+            $_.Name -notlike "Microsoft*" -and
+            $_.Name -notlike "*OneNote*" -and
+            $_.Name -notlike "*Nitro PDF*" -and
+            $_.Name -notlike "*Fax*" -and
+            $_.Name -notlike "*PDF*"
+        }
+
+        foreach ($printer in $printers) {
+            $printerName = $printer.Name
+            Write-Host "Removing Printer: $printerName"
+            Remove-Printer -Name $printerName
+        }
+
+        Write-Host "Non-default printers removed, excluding Microsoft drivers, OneNote, Nitro PDF, fax, and PDF printers."
+
+        # Enumerate all printer drivers and filter out non-Microsoft drivers to remove
+        $drivers = Get-PrinterDriver | Where-Object { $_.Name -notlike "Microsoft*" }
+
+        foreach ($driver in $drivers) {
+            $driverName = $driver.Name
+            Write-Host "Removing Printer Driver: $driverName"
+            Remove-PrinterDriver -Name $driverName
+        }
+
+        Write-Host "Non-Microsoft printer drivers removed."
+
+        # Run gpupdate /force
+        Write-Host "Running gpupdate /force..."
+        Invoke-Expression -Command "gpupdate /force"
+        Write-Host "gpupdate /force completed."
+    } catch {
+        Write-Error "An error occurred: $($_.Exception.Message)"
+    }
+}
 
 function explorer {
     # Explorer++
@@ -73,8 +116,12 @@ function explorer {
         if (Test-Path $downloadPath) {
             Write-Host "File $($downloadPath) already exists. Skipping download."
         } else {
-            Download-File -Url $url -Destination $downloadPath
-            Write-Host "Downloaded Explorer++ Portable to $($downloadPath)."
+            try {
+                Download-File -Url $url -Destination $downloadPath
+                Write-Host "Downloaded Explorer++ Portable to $($downloadPath)."
+            } catch {
+                Write-Error "Failed to download Explorer++ Portable: $($_.Exception.Message)"
+            }
         }
         if (Test-Path $downloadPath) {
             Start-Process $downloadPath
@@ -103,8 +150,12 @@ function browser {
         if (Test-Path $downloadPath) {
             Write-Host "File $($downloadPath) already exists. Skipping download."
         } else {
-            Download-File -Url $url -Destination $downloadPath
-            Write-Host "Downloaded Sea Monkey Portable to $($downloadPath)."
+            try {
+                Download-File -Url $url -Destination $downloadPath
+                Write-Host "Downloaded Sea Monkey Portable to $($downloadPath)."
+            } catch {
+                Write-Error "Failed to download Sea Monkey Portable: $($_.Exception.Message)"
+            }
         }
         if (Test-Path $downloadPath) {
             Start-Process $downloadPath
@@ -113,9 +164,6 @@ function browser {
         }
     }
 }
-
-
-
 
 function add_local_user {
     # Add local user
@@ -144,8 +192,12 @@ function add_local_user {
     $logonPasswordChange = ($logonPasswordChange.ToLower() -eq "y")
     $password = ConvertFrom-SecureString $password
     $password = [System.Text.Encoding]::Unicode.GetString($password)
-    New-LocalUser -Name $username -Password $password -AccountNeverExpires $true -PasswordNeverExpires $true -logonpasswordchange $logonPasswordChange -FullName $fullname
-    Write-Host "User $($username) has been created."
+    try {
+        New-LocalUser -Name $username -Password $password -AccountNeverExpires $true -PasswordNeverExpires $true -logonpasswordchange $logonPasswordChange -FullName $fullname
+        Write-Host "User $($username) has been created."
+    } catch {
+        Write-Error "Failed to create user: $($_.Exception.Message)"
+    }
 }
 
 
@@ -177,19 +229,26 @@ function remove_local_user {
         Write-Host "User $($userToRemove.Name) was not removed."
         return
     }
-    Remove-LocalUser -Name $userToRemove.Name
-    Write-Host "User $($userToRemove.Name) has been removed."
+    try {
+        Remove-LocalUser -Name $userToRemove.Name
+        Write-Host "User $($userToRemove.Name) has been removed."
+    } catch {
+        Write-Error "Failed to remove user: $($_.Exception.Message)"
+    }
 }
-
 
 function dont_sleep_when_lid_closed{
     # don't sleep when lid is closed
     Write-Host "Disabling sleep when lid is closed"
-    powercfg /change -monitor-timeout-ac 0
-    powercfg /change -monitor-timeout-dc 0
-    powercfg -SETACVALUEINDEX 381b4222-f694-41f0-9685-ff5bb260df2e 4f971e89-eebd-4455-a8de-9e59040e7347 5ca83367-6e45-459f-a27b-476b1d01c936 0
-    powercfg -SETDCVALUEINDEX 381b4222-f694-41f0-9685-ff5bb260df2e 4f971e89-eebd-4455-a8de-9e59040e7347 5ca83367-6e45-459f-a27b-476b1d01c936 0
-    Write-Host "Sleep when lid is closed has been disabled"
+    try {
+        powercfg /change -monitor-timeout-ac 0
+        powercfg /change -monitor-timeout-dc 0
+        powercfg -SETACVALUEINDEX 381b4222-f694-41f0-9685-ff5bb260df2e 4f971e89-eebd-4455-a8de-9e59040e7347 5ca83367-6e45-459f-a27b-476b1d01c936 0
+        powercfg -SETDCVALUEINDEX 381b4222-f694-41f0-9685-ff5bb260df2e 4f971e89-eebd-4455-a8de-9e59040e7347 5ca83367-6e45-459f-a27b-476b1d01c936 0
+        Write-Host "Sleep when lid is closed has been disabled"
+    } catch {
+        Write-Error "Failed to disable sleep when lid is closed: $($_.Exception.Message)"
+    }
 }
 
 function gpupdate {
@@ -197,16 +256,19 @@ function gpupdate {
     Updates group policy on the local computer.
     #>
     Write-Host "Updating group policy"
-    $gpupdate = Start-Process -FilePath "gpupdate.exe" -ArgumentList "/force" -PassThru -Wait
-    if ($gpupdate.ExitCode -eq 0) {
-        Write-Host "Group policy updated successfully"
-    } else {
-        Write-Host "Failed to update group policy. Creating gp report in C:\Intel."
-        $reportPath = "C:\Intel\gpupdate_report.html"
-        Get-GPReport -Path $reportPath -Domain $env:USERDNSDOMAIN -ReportType Html
+    try {
+        $gpupdate = Start-Process -FilePath "gpupdate.exe" -ArgumentList "/force" -PassThru -Wait
+        if ($gpupdate.ExitCode -eq 0) {
+            Write-Host "Group policy updated successfully"
+        } else {
+            Write-Host "Failed to update group policy. Creating gp report in C:\Intel."
+            $reportPath = "C:\Intel\gpupdate_report.html"
+            Get-GPReport -Path $reportPath -Domain $env:USERDNSDOMAIN -ReportType Html
+        }
+    } catch {
+        Write-Error "Failed to update group policy: $($_.Exception.Message)"
     }
 }
-
 
 function disable_hwa {
     <#
@@ -217,53 +279,80 @@ function disable_hwa {
     $edge_path = 'HKCU:\Software\Microsoft\Edge'
     if (Test-Path $chrome_path) {
         Write-Host "Disabling hardware acceleration for Google Chrome"
-        Set-ItemProperty -Path "$chrome_path\HardwareAccelerationModeEnabled" -Value 0
+        try {
+            Set-ItemProperty -Path "$chrome_path\HardwareAccelerationModeEnabled" -Name "ValueName" -Value 0
+        } catch {
+            Write-Error "Failed to disable hardware acceleration for Google Chrome: $($_.Exception.Message)"
+        }
     }
     if (Test-Path $firefox_path) {
         Write-Host "Disabling hardware acceleration for Mozilla Firefox"
-        Set-ItemProperty -Path "$firefox_path\Layers.acceleration.disabled" -Value 1
+        try {
+            Set-ItemProperty -Path "$firefox_path\Layers.acceleration.disabled" -Name "ValueName" -Value 1
+        } catch {
+            Write-Error "Failed to disable hardware acceleration for Mozilla Firefox: $($_.Exception.Message)"
+        }
     }
     if (Test-Path $edge_path) {
         Write-Host "Disabling hardware acceleration for Microsoft Edge"
-        Set-ItemProperty -Path "$edge_path\GPUAcceleration" -Value 0
+        try {
+            Set-ItemProperty -Path "$edge_path\GPUAcceleration" -Name "ValueName" -Value 0
+        } catch {
+            Write-Error "Failed to disable hardware acceleration for Microsoft Edge: $($_.Exception.Message)"
+        }
     }
 }
-
-
 
 function update_reboot{
     # update and reboot
     Write-Host "Updating and rebooting"
-    Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/TheTrueZeroTwo/winupdate/main/update-reboot.ps1')
+    try {
+        Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/TheTrueZeroTwo/winupdate/main/update-reboot.ps1')
+    } catch {
+        Write-Error "Failed to update and reboot: $($_.Exception.Message)"
+    }
 }
 
 function update_noreboot{
     # update and don't reboot
     Write-Host "Updating and not rebooting"
-    Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/TheTrueZeroTwo/winupdate/main/update-noreboot.ps1')
+    try {
+        Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/TheTrueZeroTwo/winupdate/main/update-noreboot.ps1')
+    } catch {
+        Write-Error "Failed to update and not reboot: $($_.Exception.Message)"
+    }
 }
 
 function bluescreen{
     # bluescreen
     Write-Host "Bluescreening"
-    Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/peewpw/Invoke-BSOD/master/Invoke-BSOD.ps1')
+    try {
+        Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/peewpw/Invoke-BSOD/master/Invoke-BSOD.ps1')
+    } catch {
+        Write-Error "Failed to bluescreen: $($_.Exception.Message)"
+    }
 }
 
 function check_system_integrity{
     # check system integrity using sfc and dism (restorehealt, component check, scanhealth)
     Write-Host "Checking system integrity"
-    sfc /scannow; dism /online /cleanup-image /restorehealth; Dism /online /Cleanup-Image /StartComponentCleanup
+    try {
+        sfc /scannow; dism /online /cleanup-image /restorehealth; Dism /online /Cleanup-Image /StartComponentCleanup
+    } catch {
+        Write-Error "Failed to check system integrity: $($_.Exception.Message)"
+    }
 }
 
 function remove_old_profiles {
     <#
     removes profiles that are older than 30 days, excluding the current user and any default Microsoft accounts, as well as any profile with "admin" in the name
     #>
-    $profiles = Get-ChildItem -Path "C:\Users" -Directory | Where-Object {
-        $_.Name -ne "Administrator" -and
-        $_.Name -notlike "*admin*" -and
-        $_.Name -ne "All Users" -and
-        $_.Name -ne "Default" -and
+    try {
+        $profiles = Get-ChildItem -Path "C:\Users" -Directory | Where-Object {
+            $_.Name -ne "Administrator" -and
+            $_.Name -notlike "*admin*" -and
+            $_.Name -ne "All Users" -and
+            $_.Name -ne "Default" -
         $_.Name -ne "Default User" -and
         $_.Name -ne "Public"
     }
@@ -276,59 +365,56 @@ function remove_old_profiles {
         $profile = $_
         $profile_age = (Get-Date) - $profile.CreationTime
         if ($profile_age.Days -gt 30 -and $profile.Name -ne $current_user) {
-            Write-Host "Removing profile $($profile.Name)"
-            Remove-Item -Path $profile.FullName -Recurse -Force
+            try {
+                Write-Host "Removing profile $($profile.Name)"
+                Remove-Item -Path $profile.FullName -Recurse -Force
+                Write-Host "Profile $($profile.Name) has been removed."
+            } catch {
+                Write-Error "Failed to remove profile $($profile.Name): $($_.Exception.Message)"
+            }
         }
     }
 }
-
 
 function reboot{
     # reboot
     Write-Host "Rebooting"
-    shutdown /r /f /T 0
+    try {
+        shutdown /r /f /T 0
+    } catch {
+        Write-Error "Failed to reboot: $($_.Exception.Message)"
+    }
 }
 
+#Keep this at the bottom of the script
+
+# Get all function names in the script
+$functionNames = Get-Command -Type Function | Select-Object -ExpandProperty Name
+
 do {
-    Show-Menu
+    Clear-Host
+    Write-Host "Menu:"
+    for ($i = 0; $i -lt $functionNames.Count; $i++) {
+        Write-Host "$($i+1). $($functionNames[$i])"
+    }
+    Write-Host "q. Quit"
+
     $selection = Read-Host "Please make a selection"
-    switch ($selection) {
-        '1' {
-            update_noreboot
-        } '2' {
-            update_reboot
-        } '3' {
-            bluescreen
-        } '4' {
-            remove_old_profiles
-        } '5' {
-            check_system_integrity
-        } '6' {
-            disable_hwa
-        } '7' {
-            dont_sleep_when_lid_closed
-        } '8' {
-            add_local_user
-        } '9' {
-            remove_local_user
-        } '10' {
-            gpupdate
-        } '11' {
-            browser
-        } '12' {
-            explorer
-        } '13' {
-            reboot
-        } 'q' {
-            break
-        }
-        default {
-            Write-Host "Invalid selection. Please try again."
-        }
+    if ($selection -eq 'q') {
+        break
     }
-    if ($selection -ne 'q') {
-        Write-Host "Press any key to continue..."
-        $null = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
-        Write-Host ""
+    if ($selection -ge 1 -and $selection -le $functionNames.Count) {
+        $selectedFunction = $functionNames[$selection - 1]
+        Write-Host "You selected: $selectedFunction"
+        
+        # Call the selected function
+        & $selectedFunction
     }
-} while ($selection -ne 'q')
+    else {
+        Write-Host "Invalid selection. Please try again."
+    }
+
+    Write-Host "Press any key to continue..."
+    $null = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+    Write-Host ""
+} while ($true)
