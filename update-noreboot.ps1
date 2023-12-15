ed226665b2d8ee26ad72a56f29877f5149f249e2
Zero Two <85081508+TheTrueZeroTwo@users.noreply.github.com>
2023-12-15 08:58:29 -0700

    Update update-noreboot.ps1
    Added winget update if winget is installed on the system

--- update-noreboot.ps1
+++ update-noreboot.ps1
@@ -1,8 +1,28 @@
-Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process -Force
-Install-PackageProvider -Name NuGet -Force
-Import-PackageProvider -Name NuGet
-Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
-Get-PSRepository -Name PSGallery | Format-List * -Force
-Install-Module -Name PSWindowsUpdate -Force
-Import-Module -Name PSWindowsUpdate
-Get-WUInstall -MicrosoftUpdate -AcceptAll -Download -Install -IgnoreReboot
+# Function to update Windows using PowerShell commands
+function Update-WindowsWithPowerShell {
+    Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process -Force
+    Install-PackageProvider -Name NuGet -Force
+    Import-PackageProvider -Name NuGet
+    Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+    Get-PSRepository -Name PSGallery | Format-List * -Force
+    Install-Module -Name PSWindowsUpdate -Force
+    Import-Module -Name PSWindowsUpdate
+    Get-WUInstall -MicrosoftUpdate -AcceptAll -Download -Install -IgnoreReboot
+}
+
+# Function to update Windows using winget
+function Update-WindowsWithWinget {
+    # Check if winget is available
+    $wingetInstalled = Get-Command -Name winget -ErrorAction SilentlyContinue
+
+    if ($wingetInstalled -ne $null) {
+        # If winget is available, use it to upgrade all packages
+        winget upgrade --all
+    } else {
+        Write-Host "winget is not installed. Run the Update-WindowsWithPowerShell function instead."
+    }
+}
+
+# Run both functions sequentially
+Update-WindowsWithPowerShell
+Update-WindowsWithWinget
