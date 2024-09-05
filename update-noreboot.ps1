5215deddc8884ae340ac34d2c80618b71efae078
Zero Two <85081508+TheTrueZeroTwo@users.noreply.github.com>
2024-09-05 16:04:03 -0600

    Update update-noreboot.ps1
    Added a function to try to install winget

--- update-noreboot.ps1
+++ update-noreboot.ps1
@@ -17,6 +17,9 @@ function Update-WindowsWithPowerShell {
 }
 
 # Function to update Windows using winget
+# Define a global variable to track the number of times winget was tried
+$global:Tried_Winget = 0
+
 function Update-WindowsWithWinget {
     # Check if winget is available
     $wingetInstalled = Get-Command -Name winget -ErrorAction SilentlyContinue
@@ -25,9 +28,83 @@ function Update-WindowsWithWinget {
         # If winget is available, use it to upgrade all packages
         winget upgrade --all --silent --accept-source-agreements --accept-package-agreements --disable-interactivity --Force
     } else {
-        Write-Host "winget is not installed. Run the Update-WindowsWithPowerShell function instead."
+        if ($global:Tried_Winget -eq 0) {
+            Write-Host "winget is not installed. Attempting to install it using Install-WinGet function."
+            # Call Install-WinGet function to install winget
+            Install-WinGet
+            # Set the counter to 1
+            $global:Tried_Winget = 1
+            # Try to run the function again after installing winget
+            Update-WindowsWithWinget
+        } else {
+            Write-Error "Failed to install winget and update packages after retrying. Exiting script."
+            return
+        }
     }
 }
+
+Function Install-WinGet {
+    #Install the latest package from GitHub
+    [cmdletbinding(SupportsShouldProcess)]
+    [alias("iwg")]
+    [OutputType("None")]
+    [OutputType("Microsoft.Windows.Appx.PackageManager.Commands.AppxPackage")]
+    Param(
+        [Parameter(HelpMessage = "Display the AppxPackage after installation.")]
+        [switch]$Passthru
+    )
+
+    Write-Verbose "[$((Get-Date).TimeofDay)] Starting $($myinvocation.mycommand)"
+
+    if ($PSVersionTable.PSVersion.Major -eq 7) {
+        Write-Warning "This command does not work in PowerShell 7. You must install in Windows PowerShell."
+        return
+    }
+
+    # Test for requirement
+    $Requirement = Get-AppPackage "Microsoft.DesktopAppInstaller"
+    if (-Not $requirement) {
+        Write-Verbose "Installing Desktop App Installer requirement"
+        Try {
+            Add-AppxPackage -Path "https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx" -ErrorAction Stop
+        }
+        Catch {
+            Throw $_
+        }
+    }
+
+    $uri = "https://api.github.com/repos/microsoft/winget-cli/releases"
+
+    Try {
+        Write-Verbose "[$((Get-Date).TimeofDay)] Getting information from $uri"
+        $get = Invoke-RestMethod -uri $uri -Method Get -ErrorAction stop
+
+        Write-Verbose "[$((Get-Date).TimeofDay)] Getting latest release"
+        $data = $get[0].assets | Where-Object name -Match 'msixbundle'
+
+        $appx = $data.browser_download_url
+        Write-Verbose "[$((Get-Date).TimeofDay)] $appx"
+        If ($pscmdlet.ShouldProcess($appx, "Downloading asset")) {
+            $file = Join-Path -path $env:temp -ChildPath $data.name
+
+            Write-Verbose "[$((Get-Date).TimeofDay)] Saving to $file"
+            Invoke-WebRequest -Uri $appx -UseBasicParsing -DisableKeepAlive -OutFile $file
+
+            Write-Verbose "[$((Get-Date).TimeofDay)] Adding Appx Package"
+            Add-AppxPackage -Path $file -ErrorAction Stop
+
+            if ($Passthru) {
+                Get-AppxPackage microsoft.desktopAppInstaller
+            }
+        }
+    }
+    Catch {
+        Write-Verbose "[$((Get-Date).TimeofDay)] There was an error."
+        Throw $_
+    }
+    Write-Verbose "[$((Get-Date).TimeofDay)] Ending $($myinvocation.mycommand)"
+}
+
 
 # Run both functions sequentially
 Update-WindowsWithPowerShell
